ffdfc65cd98dbde29d730ba85b9a325096f014c3
Joren Brunekreef <joren@dznd.nl>
2013-02-21 19:32:54 +0000

    refactor to simpler setup, matrix multiplication

--- states.js
+++ states.js
@@ -1,14 +1,20 @@
-function vector(elements) {
-	if(elements instanceof Array) { 
-		this.elements = elements; 
-		this.dim = elements.length;
+function pureState(elements) {
+
+	var _vector = elements;
+
+	this.getElements = function() {
+		return lin.normalize(_vector);
 	}
-	else return false;	
 }
 
-vector.prototype.normalize = function() {
+function lin() {
+
+
+}
+
+lin.normalize = function(vector) {
 	var total = 0;
-	var elements = this.elements;
+	var elements = vector;
 	for(var i in elements) {
 		total += elements[i];
 	}
@@ -19,45 +25,67 @@ vector.prototype.normalize = function() {
 	return elements;
 }
 
-vector.create = function(dim) { var arr = []; for(var i = 0; i < dim; i++) { arr.push(0); } return new vector(arr); };
 
-function pureState(elements) {
 
-	var _vector = new vector(elements);
-
-	this.getElements = function() {
-		return _vector.normalize();
-	}
-}
-
-function lin() {
-
-		
-
-}
-
-lin.innerProduct = function(s1, s2) {
-	if(s1.getElements().length != s2.getElements().length) { return false; }
+lin.vectorDot = function(s1, s2) {
+	if(s1.length != s2.length) { return false; }
 	var inP = 0;
-	for(var i = 0; i < s1.getElements().length; i++) {
-		inP += s1.getElements()[i] * s2.getElements()[i];
+	for(var i = 0; i < s1.length; i++) {
+		inP += s1[i] * s2[i];
 	}
 	return inP;
 }
 
+lin.matrixDot = function(m, v) {
+	if(m.length != v.length) return false;
+
+	var out = [];
+	for(var i in m) {
+		var el = 0;
+		for(var j in m[i]) {
+			el += m[i][j] * v[j];
+		}
+		out.push(el);
+	}
+
+	return out;
+}
+
+lin.dot = function(a, b) {
+	if(lin.isMatrix(a) && lin.isVector(b)) return lin.matrixDot(a,b);
+	else return lin.vectorDot(a,b);
+}
+
 lin.tensorProduct = function(s1, s2) {
-	var tensorP = vector.create(s1.dim * s2.dim);
-	for(var i = 0; i < s1.dim; i++) {
-		for(var j = 0; j < s2.dim; j++) {
-			tensorP.elements[s2.dim*i+j] = s1.elements[i] * s2.elements[j];
+	var tensorP = [];
+	for(var i = 0; i < s1.length; i++) {
+		for(var j = 0; j < s2.length; j++) {
+			tensorP.push(s1[i] * s2[j]);
 		}
 	}
 
 	return tensorP;
 }
 
-/* test */
 
-var p1 = new vector([1,2]);
-var p2 = new vector([2,3]);
 
+lin.isVector = function(candidate) {
+	if(!(candidate instanceof Array)) return false;
+	return true;
+}
+
+lin.isMatrix = function(candidate) {
+	if(!(candidate instanceof Array)) return false;
+	if(!(candidate[0] instanceof Array)) return false;
+	var length = candidate[0].length;
+	for(var i in candidate) {
+		if(!(candidate[i] instanceof Array)) return false 
+		if(candidate[i].length != length) return false;	
+	}
+	return true;
+}
+
+var p1 = [1,2];
+var p2 = [2,3];
+var mid = [[1,0],[0,1]];
+var mtr = [[0,1],[1,0]];
