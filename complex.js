34af5a69c3bd7db410000bb6fc6a5be749766c1a
Joren Brunekreef <joren@dznd.nl>
2013-02-27 17:26:07 +0000

    complex multiplication, modulus and string representation

--- complex.js
+++ complex.js
@@ -33,7 +33,24 @@ c.prototype = {
 		} else if(arguments.length == 2) {
 			return new c(this.re - arguments[0], this.im - arguments[1]);
 		}
-	}
+	},
 
+	multiply: function() {
+		var m = arguments[0];
+		if(arguments.length == 1 && typeof(arguments[0]) == 'number') m = new c(arguments[0], 0);
+		else if(arguments.length == 2) m = new c(arguments[0], arguments[1]);
 
+		return new c(this.re * m.re - this.im * m.im, this.re * m.im + this.im * m.re);
+	},
+
+	modulus: function() {
+		return Math.sqrt(this.re*this.re + this.im*this.im);
+	},
+
+	toString: function() {
+		var str = '';
+		str += this.re;
+		str += this.im < 0 ? this.im +'i': '+' + this.im + 'i';
+		return str;
+	}	
 }
